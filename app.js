bfea4688ce8a1be69d8ce1e14aa0bc7c0a860d85
Jorge Teixeira <jorgemrt@protonmail.com>
2020-03-02 19:10:40 +0000

    deploy linode

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Body Parser
 app.use(express.json({ extended: false }));
 
-app.use(express.static('public'))
+// Set static folder
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 module.exports = app;
